7b89ef1ee40cfc9815389594ad2c37e469d72443
git <sven.uhlig@tiq-solutions.de>
2019-06-05 17:59:46 +0200

    remove unused o.e.e.http.jetty fragment

--- org.eclipse.equinox.http.jetty.websocket/src/org/eclipse/equinox/http/jetty/websocket/package-info.java
+++
@@ -1,6 +0,0 @@
-@Header(name = Constants.FRAGMENT_HOST,value = "org.eclipse.equinox.http.jetty")
-package org.eclipse.equinox.http.jetty.websocket;
-
-import org.osgi.annotation.bundle.Header;
-import org.osgi.framework.Constants;
-
