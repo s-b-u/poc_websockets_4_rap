e8f119d82aa9a3f7ac7c2b74c0095ca8e6af3bd3
git <s-b-u@gmx.net>
2019-06-28 09:45:02 +0200

    add EMF Rap Demo

---
+++ org.eclipse.emf.ecp.makeithappen.application.sample.rap/src/org/eclipse/emf/ecp/makeithappen/application/sample/rap/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2011-2014 EclipseSource Muenchen GmbH and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ * EclipseSource Munich - initial API and implementation
+ */
+package org.eclipse.emf.ecp.makeithappen.application.sample.rap;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+/**
+ * The ApplicationWorkbenchAdvisor which defines which Perspective to use.
+ */
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "TestApp.perspective"; //$NON-NLS-1$
+
+	@Override
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+		IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+
+	@Override
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+
+}
